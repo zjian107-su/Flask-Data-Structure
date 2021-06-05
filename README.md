532d1e4bc11daf590072123e785cb2079e2107a5
Daniel Jiang <danieljc8888@gmail.com>
2021-06-05 07:09:36 -0400

    linked list, dbfile

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # flask
 
 ## Learning Link on YouTube(17:08) - https://www.youtube.com/watch?v=74NW-84BqbA 
+
+## Codes 
+- Generate db file: `from server import db`, `db.create_all()`, `exit()`
--- server.py
+++ server.py
@@ -34,4 +34,73 @@ class User(db.Model):
     email = db.Column(db.String(50))
     address = db.Column(db.String(200))
     phone = db.Column(db.String(50))
-    post = db.relationship("")
+    post = db.relationship("BlogPost")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+# routes
+
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        address=data["address"],
+        phone=data["phone"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 200
+
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_users_desending():
+    pass
+
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_users_asending():
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_all_blog_posts(user_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_one_blog_post(blog_post_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_blog_post(blog_post_id):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
