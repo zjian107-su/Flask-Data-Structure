2e0da7d3be93be67277d0743f6648a1d195ca89f
Daniel Jiang <danieljc8888@gmail.com>
2021-07-09 10:25:38 -0400

    custom_q to get numeric_q sum

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 import linked_list
 import binary_search_tree
 
+import custom_q
 import random
 
 # app
@@ -178,9 +179,35 @@ def get_all_blog_posts(blog_post_id):
     return jsonify(post)
 
 
-@app.route("/blog_post/<blog_post_id>", methods=["GET"])
-def get_one_blog_post(blog_post_id):
-    pass
+@app.route("/blog_post/numeric_body", methods=["GET"])
+def get_numeric_post_bodies():
+    blog_posts = BlogPost.query.all()
+
+    q = custom_q.Queue()
+
+    for post in blog_posts:
+        q.enqueue(post)
+
+    return_list = []
+
+    for _ in range(len(blog_posts)):
+        post = q.dequeue()
+        numeric_body = 0
+        for char in post.data.body:
+            numeric_body += ord(char)
+
+        post.data.body = numeric_body
+
+        return_list.append(
+            {
+                "id": post.data.id,
+                "title": post.data.title,
+                "body": post.data.body,
+                "user_id": post.data.user_id,
+            }
+        )
+
+    return jsonify(return_list)
 
 
 @app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
